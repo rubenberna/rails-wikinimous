3fe19b1470d8e44a468adf238df01d068a37d4ba
ruben bernardes <rubenmbernardes@gmail.com>
2018-03-13 17:45:53 +0100

    skeleton - cleaned routes created by the controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-  get '/index'
-
-  get '/create'
-
-  get '/new'
-
-  get '/edit'
-
-  get '/show'
-
-  get '/update'
-
-  get '/destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
 end
